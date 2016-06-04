3dd209164bdd7943e4d130492bd99b2da92677c1
dwulff <dirk.wulff@gmail.com>
2016-06-05 00:21:38 +0200

    Update server.R

--- MFA/server.R
+++ MFA/server.R
@@ -1,7 +1,7 @@
 shinyServer(function(input, output) {
 
  if(!require(devtools,quietly=T)) install.packages('devtools') ; require(devtools)
- if(!require(shinyCpp,quitely=T)) install_github('dwulff/JDMxWorkshop/shinyCpp')
+ if(!require(shinyCpp,quietly=T)) install_github('dwulff/JDMxWorkshop/shinyCpp')
 
  require(shinyCpp)
   
